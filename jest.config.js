3e7048db8afd9b1d4483ede67f86765c7e8040da
Maulik <maulik.joshi@bacancy.com>
2025-07-27 11:27:14 +0530

    chore: initial project setup with Vite

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+import { createDefaultPreset } from "ts-jest";
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+export default {
+  testEnvironment: "jsdom",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  testMatch: [
+    "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
+    "<rootDir>/src/**/*.{test,spec}.{js,jsx,ts,tsx}",
+  ],
+};
